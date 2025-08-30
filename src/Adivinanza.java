fb2b3a26a8e6e339a2d5e3a4c43befbccdbe97ac
Samuel <samuelbonifacio015@gmail.com>
2025-08-30 15:17:01 -0500

    feat: adivinanza added

---
+++ src/Adivinanza.java
@@ -0,0 +1,8 @@
+import java.util.Random;
+
+public class Adivinanza {
+    public static void main (String[] args){
+        Random randomNumbers = new Random();
+        System.out.println("Ingresa un numero: ");
+    }
+}
